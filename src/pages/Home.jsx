77005195374fef85ad2f257dc00dabccfe99a576
Sudip Acharya <drxking456@gmail.com>
2024-08-06 08:39:10 +0545

    EveryThing fine till here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Respo from "../components/Respo";
 import Skills from "../components/Skills";
 import { useState } from "react";
 import Projects from "../components/Projects";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
@@ -13,7 +12,6 @@ const Home = () => {
   };
   return (
     <div className="lg:px-20 md:px-10 px-4 text-[--text-color] duration-300 overflow-x-hidden">
-
       <Respo  />
       <Navbar />
       <Hero onImageLoad={handleImageLoad} />
