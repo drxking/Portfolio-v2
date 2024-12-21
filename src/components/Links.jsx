848cd65ddc5dd5b15c70dfcc03dd43c45a91bda4
Sudip Acharya <drxking456@gmail.com>
2024-12-21 22:41:12 +0545

    Corrected Links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const social = Object.entries({
   "ri-facebook-circle-fill": "https://www.facebook.com/sudip.acharya.927980",
-  "ri-instagram-fill": "https://www.instagram.com/sudipacharya888/",
+  "ri-instagram-fill": "https://www.instagram.com/sudipacharya.js/",
   "ri-twitter-x-fill": "https://twitter.com/drxking456",
   "ri-linkedin-box-fill":
     "https://www.linkedin.com/in/sudip-acharya-937347281/",
   "ri-github-fill": "https://github.com/drxking",
-  "ri-threads-fill": "https://www.threads.net/@sudipacharya888",
+  "ri-threads-fill": "https://www.threads.net/@sudipacharya.js",
 });
 
 const Links = () => {
